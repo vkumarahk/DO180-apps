2d0a617df521ebc3c0deb83fdfd81e2af9f45a38
Student User <student@workstation.lab.example.com>
2021-01-21 18:22:20 -0500

    corrected the error

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "New Change will be come soon! author:Vinayhk" "\n";
+print "New Change will be come soon! author:Vinayhk". "\n";
 ?>
